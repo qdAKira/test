6c567ce534384beac4be6ac2a8b5e54f0826e947
qdAkira <995538274@qq.com>
2022-10-25 16:27:34 +0800

    10/25

--- src/settings.js
+++ src/settings.js
@@ -1,12 +1,12 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: '南通平台在线运维',
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: false,
+  fixedHeader: true,
 
   /**
    * @type {boolean} true | false
